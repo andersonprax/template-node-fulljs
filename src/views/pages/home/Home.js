d352ac06229faa86ceff30297a9b681307ed1787
Anderson Praxedes Soares <andersonintendope@gmail.com>
2022-10-26 19:23:43 -0300

    feat: Add Gama Academy project
    Gama Academy Project about Node end full JS

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,10 +1,23 @@
+import api from '../../../service/api';
+
+async function getRandomJoke(){
+  const request = await api.get('random')
+  const response = request.data
+  return response
+}
+
+
+
 let Home = {
   is_private: false,
 
   render: async () => {
-      let view = `
+    const jokes = await getRandomJoke();
+      let view = /*html*/`
           <div>
-            <h1>Home</div>
+            <h1>Primeira página</h1>
+            <img src=${jokes.icon_url} alt=${jokes.value}>
+            <p>${jokes.value}</p>
           </div>
       `;
 
